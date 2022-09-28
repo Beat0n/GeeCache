409754bd92c34d9ac04ec4cbcd9c38b48a73dc5b
胡冀哲 <hujizheuestc@163.com>
2022-09-28 10:17:24 +0800

    Initial commit

---
+++ lru/lru.go
@@ -0,0 +1,43 @@
+package lru
+
+import "container/list"
+
+type Cache struct {
+	maxBytes  int64
+	nbytes    int64
+	ll        *list.List
+	cache     map[string]*list.Element
+	onEvicted func(key string, value Value)
+}
+
+type Value interface {
+	Bytes() int
+}
+
+type entry struct {
+	key   string
+	value Value
+}
+
+func New(maxBytes int64, onEvicted func(string, Value)) *Cache {
+	return &Cache{
+		maxBytes:  maxBytes,
+		nbytes:    0,
+		ll:        list.New(),
+		onEvicted: onEvicted,
+		cache:     make(map[string]*list.Element),
+	}
+}
+
+func (c *Cache) Get(key string) (value Value, ok bool) {
+	if ele, ok := c.cache[key]; ok {
+		c.ll.MoveToFront(ele)
+		kv := ele.Value.(entry)
+		return kv.value, true
+	}
+	return
+}
+
+func (c *Cache) RemoveOldest() {
+	ele := 
+}
