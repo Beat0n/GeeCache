657590f2e18bf51a97c6b6f270a51baad633edd7
Beat0n <hujizheuestc@163.com>
2022-09-28 12:33:40 +0800

    ""

--- lru/lru.go
+++ lru/lru.go
@@ -29,15 +29,45 @@ func New(maxBytes int64, onEvicted func(string, Value)) *Cache {
 	}
 }
 
+// Get look ups a key's value
 func (c *Cache) Get(key string) (value Value, ok bool) {
 	if ele, ok := c.cache[key]; ok {
 		c.ll.MoveToFront(ele)
-		kv := ele.Value.(entry)
+		kv := ele.Value.(*entry)
 		return kv.value, true
 	}
 	return
 }
 
 func (c *Cache) RemoveOldest() {
-	ele := 
+	ele := c.ll.Back()
+	if ele != nil {
+		c.ll.Remove(ele)
+		kv := ele.Value.(*entry)
+		delete(c.cache, kv.key)
+		c.nbytes -= int64(len(kv.key)) + int64(kv.value.Bytes())
+		if c.onEvicted != nil {
+			c.onEvicted(kv.key, kv.value)
+		}
+	}
+}
+
+func (c *Cache) Add(key string, value Value) {
+	if ele, ok := c.cache[key]; ok {
+		c.ll.MoveToFront(ele)
+		kv := ele.Value.(*entry)
+		c.nbytes += int64(value.Bytes()) - int64(kv.value.Bytes())
+		kv.value = value
+	} else {
+		ele := c.ll.PushFront(&entry{key, value})
+		c.cache[key] = ele
+		c.nbytes += int64(len(key)) + int64(value.Bytes())
+	}
+	for c.maxBytes != 0 && c.maxBytes < c.nbytes {
+		c.RemoveOldest()
+	}
+}
+
+func (c *Cache) Len() int {
+	return c.ll.Len()
 }
